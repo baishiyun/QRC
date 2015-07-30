bb94b2433b814ffd0cc4b550c87842cccf2a1f54
baishiyun <769290632@qq.com>
2015-07-30 12:59:31 +0800

    iOS 原生态API实现二维码扫码功能

---
+++ QRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRC
+//
+//  Created by chenchen on 15/7/30.
+//  Copyright (c) 2015年 BSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
